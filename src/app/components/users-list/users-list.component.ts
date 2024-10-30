0a479664c3b1ae15fb6eb6bc69ab62690bd567d4
AbuAbdulmalik <mr.betterdoctor@gmail.com>
2024-10-30 17:52:48 +0300

    add UsersApiService

--- src/app/components/users-list/users-list.component.ts
+++ src/app/components/users-list/users-list.component.ts
@@ -3,7 +3,7 @@ import { NgFor } from '@angular/common';
 
 import { HeaderComponent } from '../header/header.component';
 import { User } from '../../models/users.model';
-import {HttpClient} from "@angular/common/http";
+import { UsersApiService } from '../../services/users-api.service';
 
 @Component({
   selector: 'app-users-list',
@@ -13,24 +13,22 @@ import {HttpClient} from "@angular/common/http";
   styleUrl: './users-list.component.scss',
 })
 export class UsersListComponent {
-  readonly apiService = inject(HttpClient)
+  readonly usersApiService = inject(UsersApiService);
   users: User[] = [];
 
   constructor() {
-    this.apiService.get('https://jsonplaceholder.typicode.com/users').subscribe(
-      (response: any) => {
-        this.users = response
-        console.log(response)
-      }
-    )
+    this.usersApiService.getUsers().subscribe((response: any) => {
+      this.users = response;
+      console.log(response);
+    });
   }
 
   deleteUser(id: number, name: string) {
-    const confirmed = window.confirm(`Вы действительно хотите удалить пользователя ${name}?`);
-    if(confirmed) {
-      this.users = this.users.filter(
-        user => user.id !== id
-      )
+    const confirmed = window.confirm(
+      `Вы действительно хотите удалить пользователя ${name}?`
+    );
+    if (confirmed) {
+      this.users = this.users.filter((user) => user.id !== id);
     }
   }
 }
